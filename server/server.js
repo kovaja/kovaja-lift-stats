6c2edc47bbae05b8efb883a734da0803fb5e762b
Jakub Kovarik <jakubkovarik@seznam.cz>
2018-08-17 00:55:40 +0200

    http changes for server

--- server/server.js
+++ server/server.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const PORT = 3000;
+const PORT = 8000;
 const BUILD_PATH = path.join(__dirname, '../client/build');
 
 app.use(express.static(BUILD_PATH));
 
 app.get('/', function (req, res) {
   res.sendFile(BUILD_PATH+'/index.html');
+});
+
+app.post('/api', function (req, res) {
+  res.send('ahooj');
 });
 
 app.listen(PORT, () => console.log('Lift-stat server listening on ' + PORT));
