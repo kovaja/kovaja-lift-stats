7a383b5d9140e9a7c719571b0bbdd7e1dee9a6b4
Jakub Kovarik <jakubkovarik@seznam.cz>
2018-08-16 23:20:56 +0200

    Basic css and bacis markup

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,13 +4,21 @@ import { Link } from 'react-router-dom'
 export default class Header extends Component {
   render() {
     return (
-      <div className="row header">
-        <div>
-          <h1>LIFT STATISTICS</h1>
-        </div>
-        <div>
-          <Link to='/'>Ride</Link>
-          <Link to='/statistics'>Statistics</Link>
+      <div className="row">
+        <div className="col-md-12">
+          <div className="page-header">
+            <h1>
+              Lift-stats! <small>Smart way how to be first in the right lift</small>
+            </h1>
+          </div>
+          <ul className="nav nav-tabs">
+            <li className="nav-item">
+              <Link className="nav-link active" to='/'>Ride</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to='/statistics'>Statistics</Link>
+            </li>
+          </ul>
         </div>
       </div>
     );
